be0d2b8438daf7b7475ee2d36cc6325788c9abcc
unknown <sohamlanke@gmail.com>
2022-04-20 10:58:51 +0530

    auto commit

--- lib/Screens/homepage.dart
+++ lib/Screens/homepage.dart
@@ -1,35 +1,194 @@
+// ignore_for_file: prefer_const_constructors, duplicate_ignore, unnecessary_new
+
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 Widget homepage(context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text("Feeds"),
-      centerTitle: true,
-      actions: <Widget>[
-        IconButton(
-          // ignore: prefer_const_constructors
-          icon: Icon(
-            Icons.filter_list,
-          ),
-          onPressed: () {},
-        ),
-      ],
-    ),
-    body: ListView.builder(
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      itemCount: 10,
-      itemBuilder: (BuildContext context, int index) {
-        // Map post = posts[index];
-        return Text('Post here');
-      },
-    ),
-    floatingActionButton: FloatingActionButton(
-      // ignore: prefer_const_constructors
-      child: Icon(
-        Icons.add,
-        color: Colors.white,
-      ),
-      onPressed: () {},
-    ),
+   final topBar = new AppBar(
+    backgroundColor: new Color(0xfff8faf8),
+    centerTitle: true,
+    elevation: 1.0,
+    leading: new Icon(Icons.camera_alt),
+    title: SizedBox(
+        height: 35.0, child: Image.asset("assets/images/insta_logo.png")),
+    // ignore: prefer_const_literals_to_create_immutables
+    actions: <Widget>[
+      Padding(
+        padding: const EdgeInsets.only(right: 12.0),
+        child: Icon(Icons.send),
+      )
+    ],
   );
+
+  
+  return Scaffold(
+        appBar: topBar,
+        body: new InstaBody(),
+        bottomNavigationBar: new Container(
+          color: Colors.white,
+          height: 50.0,
+          alignment: Alignment.center,
+        ));
+
+
+}
+
+class InstaBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        // Expanded(flex: 1, child: new InstaStories()),
+        Flexible(child: InstaList())
+      ],
+    );
+  }
+}
+
+class InstaList extends StatefulWidget {
+  @override
+  _InstaListState createState() => _InstaListState();
+}
+
+class _InstaListState extends State<InstaList> {
+  bool isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    var deviceSize = MediaQuery.of(context).size;
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => index == 0
+          ? new SizedBox(
+              // child: new InstaStories(),
+              height: deviceSize.height * 0.15,
+            )
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          new Container(
+                            height: 40.0,
+                            width: 40.0,
+                            decoration: new BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: new DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image: new NetworkImage(
+                                      "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg")),
+                            ),
+                          ),
+                          new SizedBox(
+                            width: 10.0,
+                          ),
+                          new Text(
+                            "imthpk",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      new IconButton(
+                        icon: Icon(Icons.more_vert),
+                        onPressed: null,
+                      )
+                    ],
+                  ),
+                ),
+                Flexible(
+                  fit: FlexFit.loose,
+                  child: new Image.network(
+                    "https://images.pexels.com/photos/672657/pexels-photo-672657.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          new IconButton(
+                            icon: new Icon(isPressed
+                                ? Icons.favorite
+                                : FontAwesomeIcons.heart),
+                            color: isPressed ? Colors.red : Colors.black,
+                            onPressed: () {
+                              setState(() {
+                                isPressed = !isPressed;
+                              });
+                            },
+                          ),
+                          new SizedBox(
+                            width: 16.0,
+                          ),
+                          new Icon(
+                            FontAwesomeIcons.comment,
+                          ),
+                          new SizedBox(
+                            width: 16.0,
+                          ),
+                          new Icon(FontAwesomeIcons.paperPlane),
+                        ],
+                      ),
+                      new Icon(FontAwesomeIcons.bookmark)
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    "Liked by sohamlanke, vaishnavi, saloni and 528,331 others",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      new Container(
+                        height: 40.0,
+                        width: 40.0,
+                        decoration: new BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: new DecorationImage(
+                              fit: BoxFit.fill,
+                              image: new NetworkImage(
+                                  "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg")),
+                        ),
+                      ),
+                      new SizedBox(
+                        width: 10.0,
+                      ),
+                      Expanded(
+                        child: new TextField(
+                          decoration: new InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Add a comment...",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child:
+                      Text("1 Day Ago", style: TextStyle(color: Colors.grey)),
+                )
+              ],
+            ),
+    );
+  }
 }
